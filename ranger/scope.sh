79b655bd1729d7d6d3d47d2593cfc331c9bd4fc5
Carlo Federico Pauletti <cpaulett@sissa.it>
2022-12-12 14:13:00 +0100

    fixed ranger preview memory leak

--- ranger/scope.sh
+++ ranger/scope.sh
@@ -127,16 +127,16 @@ handle_image() {
 
     local mimetype="${1}"
     case "${mimetype}" in
-        ## SVG
-        # image/svg+xml|image/svg)
-        #     convert -- "${FILE_PATH}" "${IMAGE_CACHE_PATH}" && exit 6
-        #     exit 1;;
+        # SVG
+         image/svg+xml|image/svg)
+             convert -- "${FILE_PATH}" "${IMAGE_CACHE_PATH}" && exit 6
+             exit 1;;
 
-        ## DjVu
-        # image/vnd.djvu)
-        #     ddjvu -format=tiff -quality=90 -page=1 -size="${DEFAULT_SIZE}" \
-        #           - "${IMAGE_CACHE_PATH}" < "${FILE_PATH}" \
-        #           && exit 6 || exit 1;;
+        # DjVu
+         image/vnd.djvu)
+             ddjvu -format=tiff -quality=90 -page=1 -size="${DEFAULT_SIZE}" \
+                   - "${IMAGE_CACHE_PATH}" < "${FILE_PATH}" \
+                   && exit 6 || exit 1;;
 
         ## Image
         image/*)
@@ -170,15 +170,15 @@ handle_image() {
                  && exit 6 || exit 1;;
 
 
-        ## ePub, MOBI, FB2 (using Calibre)
-        # application/epub+zip|application/x-mobipocket-ebook|\
-        # application/x-fictionbook+xml)
-        #     # ePub (using https://github.com/marianosimone/epub-thumbnailer)
-        #     epub-thumbnailer "${FILE_PATH}" "${IMAGE_CACHE_PATH}" \
-        #         "${DEFAULT_SIZE%x*}" && exit 6
-        #     ebook-meta --get-cover="${IMAGE_CACHE_PATH}" -- "${FILE_PATH}" \
-        #         >/dev/null && exit 6
-        #     exit 1;;
+        # ePub, MOBI, FB2 (using Calibre)
+         application/epub+zip|application/x-mobipocket-ebook|\
+         application/x-fictionbook+xml)
+             # ePub (using https://github.com/marianosimone/epub-thumbnailer)
+             epub-thumbnailer "${FILE_PATH}" "${IMAGE_CACHE_PATH}" \
+                 "${DEFAULT_SIZE%x*}" && exit 6
+             ebook-meta --get-cover="${IMAGE_CACHE_PATH}" -- "${FILE_PATH}" \
+                 >/dev/null && exit 6
+             exit 1;;
 
         ## Font
         application/font*|application/*opentype)
